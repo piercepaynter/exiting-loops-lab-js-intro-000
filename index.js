cf1ad77957d7cc4efc72a3071a336097a0457032
Pierce Paynter <paynterpierce@gmail.com>
2020-06-26 03:39:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function keepGoing(array, changeValue, skipValue){
 function findBy(array, findFn){
   for (let i=0; i<array.length; i++){
     if (array[i] === findFn){
-      continue;
+      return findFn;
     }
   }
 }
