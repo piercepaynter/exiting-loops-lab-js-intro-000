ace5d4b002ba867f16271db02a5ef1639ff451c9
Pierce Paynter <paynterpierce@gmail.com>
2020-06-26 04:13:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function keepGoing(array, changeValue, skipValue){
 
 function findBy(array, findFn){
   for (let i=0; i<array.length; i++){
-    if (typeof array[i] !== "number"){
+    if (typeof array[i] !=== "number"){
       return findFn;
       break;
     }
